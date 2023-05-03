2c624233ea207552660f0c1fd7df2a52d5406ca6
silver <ekdldkaa@naver.com>
2023-05-03 15:47:47 +0900

    todo_list 완성

--- s3_todo/index.js
+++ s3_todo/index.js
@@ -1,15 +1,18 @@
+let cnt = 0;
 const submit = () => {
+    cnt+=1;
+
     console.log("함수실행");
-    let item1 = document.getElementById("item1")
-    let item2 = document.getElementById("item2")
-    let item3 = document.getElementById("item3")
+    let item = document.getElementById("item").value;
+    let todo_list= document.getElementById("todo_list");
 
-    let elem1 = document.getElementById("elem1")
-    let elem2 = document.getElementById("elem2")
-    let elem3 = document.getElementById("elem3")
+    //요소 생성
+    let list_elem = document.createElement("div")
+    list_elem.className="elem";
+    list_elem.id="elem"+cnt;
+    console.log(list_elem.className);
+    list_elem.innerText = "- "+item
 
-    elem1.innerText = '1. '+item1.value;
-    elem2.innerText = '2. '+item2.value;
-    elem3.innerText = '3. '+item3.value;
-
+    //요소 추가
+    todo_list.appendChild(list_elem);
 }
